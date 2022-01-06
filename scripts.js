6827ae21d3a04fd3e336c074de7c818c2312c2a9
wmyersta <wmyersta@adobe.com>
2022-01-06 11:14:23 -0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+<script src="//images-tv.adobe.com/cdn/mpc/build/v3_188/js/vendor.js"></script>
+<script src="//images-tv.adobe.com/cdn/mpc/build/v3_188/js/v-player.js"></script>
+<script>_satellite["_runScript1"](function(event, target, Promise) {
+var l_videoParentPageUrl=digitalData._get("digitalData.initialPage.topFrameInfo.location.href");l_videoParentPageUrl&&-1!==l_videoParentPageUrl.indexOf("adobe.")&&parent.postMessage({type:"video_meta",video_info:digitalData._get("digitalData.video.videoInfo")},l_videoParentPageUrl);
+});</script>
